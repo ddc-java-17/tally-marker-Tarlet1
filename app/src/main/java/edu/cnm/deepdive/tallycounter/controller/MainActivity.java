428f614330e6245028493d28a54a8eae48efbb1a
Timothy Arlet <tarlet@cnm.edu>
2024-02-05 13:42:56 -0700

    Save and restore instance state.

--- app/src/main/java/edu/cnm/deepdive/tallycounter/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/tallycounter/controller/MainActivity.java
@@ -5,9 +5,11 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import edu.cnm.deepdive.tallycounter.R;
+import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,17 +24,74 @@ public class MainActivity extends AppCompatActivity {
     Log.d(TAG, "MainActivity::onCreate");
     setContentView(R.layout.activity_main);
     tally = findViewById(R.id.tally);
-    tally.setText("0");
+    if (savedInstanceState !=null) {
+      setCounter(savedInstanceState.getInt("counter", 0));
+    } else {
+      setCounter(0);
+    }
     Button increment = findViewById(R.id.increment);
     increment.setOnClickListener(new IncrementListener());
+  }
+
+  @Override
+  protected void onRestart() {
+    super.onRestart();
+    Log.d(TAG, "MainActivity::onRestart");
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    Log.d(TAG, "MainActivity::onStart");
+  }
+
+  @Override
+  protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+    super.onRestoreInstanceState(savedInstanceState);
+    Log.d(TAG, "MainActivity::onRestoreInstanceState");
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    Log.d(TAG, "MainActivity::onResume");
+  }
+
+  @Override
+  protected void onPause() {
+    Log.d(TAG, "MainActivity::onPause");
+    super.onPause();
+  }
+
+  @Override
+  protected void onStop() {
+    Log.d(TAG, "MainActivity::onStop");
+    super.onStop();
+  }
+
+  @Override
+  protected void onSaveInstanceState(@NonNull @NotNull Bundle outState) {
+    super.onSaveInstanceState(outState);
+    Log.d(TAG, "MainActivity::onSaveInstanceState");
+    outState.putInt("counter", counter);
+  }
+
+  @Override
+  protected void onDestroy() {
+    Log.d(TAG, "MainActivity::onDestroy");
+    super.onDestroy();
+  }
+
+  private void setCounter(int counter) {
+  this.counter = counter;
+  tally.setText(String.valueOf(counter));
   }
 
   private class IncrementListener implements OnClickListener {
 
     @Override
     public void onClick(View v) {
-      counter++;
-      tally.setText(String.valueOf(counter));
+     setCounter(counter + 1);
     }
 
   }
